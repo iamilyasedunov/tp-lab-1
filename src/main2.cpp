1ba7525f0cbdf40542927b2b2d6b39a9d77cb640
iamilyasedunov <iliasedunov@yandex.ru>
2018-10-23 03:27:55 +0300

    B18 with TASK...

--- src/main2.cpp
+++ src/main2.cpp
@@ -2,19 +2,21 @@
 #include <iostream>
 
 int main(){
+	/*
 	unsigned int value; //проверка числа на простоту
-	std::cout<<"Enter value: ";
-	std::cin>>value;
-	std::cout<<" "<<checkPrime(value)<<endl;
+	cout<<"Enter value: ";
+	cin>>value;
+	cout<<" "<<checkPrime(value)<<endl;
 	
 	unsigned n; //нахождение n-ого простого числа (в ряду)
-	std::cout<<"Enter n: ";
-	std::cin>>n;
-	std::cout<<" "<<nPrime(n)<<endl;
+	cout<<"Enter n: ";
+	cin>>n;
+	cout<<" "<<nPrime(n)<<endl;
 	
 	unsigned int value; //нахождение ближайшего следующего простого числа к value.
-	std::cout<<"Enter value: ";
-	std::cin>>value;
-	std::cout<<" "<<nextPrime(value)<<endl;
+	cout<<"Enter value: ";
+	cin>>value;
+	cout<<" "<<nextPrime(value)<<endl;
 	return 0;
+	*/
 }
