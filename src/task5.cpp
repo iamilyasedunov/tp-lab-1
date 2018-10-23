d04fecb06da7bef4e782ee46abe1486aa60fed1b
iamilyasedunov <iliasedunov@yandex.ru>
2018-10-23 13:05:02 +0300

    task 5

--- src/task5.cpp
+++ src/task5.cpp
@@ -1,7 +1,7 @@
 #include <task5.h>
 
 void split(char ***result, int *N, char *buf, char ch){
-	size_t n = strlen(buf);
+    size_t n = strlen(buf);
     int count = 0;
     unsigned long long int maxsize = 0;
 
@@ -14,19 +14,35 @@ void split(char ***result, int *N, char *buf, char ch){
         }
         if((buf[i] == ch)||(i == n - 1)){
             count++;
-            //cout<<str<<" "<<count<<" "<<maxsize<<endl;
+            //cout<<str<<" "<<count<<" ";
             if (str.size() > maxsize){
                 maxsize = str.size();
             }
             vec.push_back(str);
+            //cout<<endl;
             str.clear();
         }
     }
 
+    *N = count;
 
 
-    (result) = new char **[count + 1]; //массив указателей
-    /*
+    char **tmp = new char*[*N];
+
+    for (int i = 0;i < vec.size(); i++) {
+        cout<<(vec[i].length())<<endl;
+        tmp[i] = new char[vec[i].length() + 1];
+        int j;
+        for (j = 0;j < vec[i].length(); j++) {
+            tmp[i][j] = vec[i][j];
+            //cout<<tmp[i]<<" "<<(vec[i].length())<<endl;
+        }
+        tmp[i][j] = '\0';
+    }
+    //cout<<endl;
+    *result = &(tmp[0]);
+
+     /*
     result[0] = new char *[maxsize + 1];
     (result)[0][0] = &(vec[0][0]);
     cout<<*(result)[0]<<" "<<(vec[0])<<endl;
@@ -38,11 +54,12 @@ void split(char ***result, int *N, char *buf, char ch){
     result[2] = new char *[maxsize + 1];
     (result)[2][0] = &(vec[2][0]);
     cout<<*(result)[2]<<" "<<(vec[2])<<endl;
-    */
+
     for (int i = 0; i < count; i++){
-        result[i] = new char *[maxsize + 1];
+        *result[i] = new char [maxsize + 1];
         (result)[i][0] = &(vec[i][0]);
-        //cout<<*(result)[i]<<" "<<vec[i]<<endl;
+        cout<<*(result)[i]<<" "<<vec[i]<<endl;
     }
+      */
     *N = count;
 }
