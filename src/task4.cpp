2959a477c23b59e5e7d22c3e7f25b3727172244c
iamilyasedunov <iliasedunov@yandex.ru>
2018-10-23 11:16:37 +0300

    task 4 first test

--- src/task4.cpp
+++ src/task4.cpp
@@ -12,36 +12,37 @@ char * sum(char *x, char *y){
 
     }
     int n = x1.size() - y1.size();
-
+    //cout<<endl<<n<<" "<<x1.size()<<" "<<y1.size()<<endl;
     if (n > 0){
         for (int i = 0; i < abs(n); i++){
             y1.push_back('0');
         }
     }
-    else{
+    if (n < 0){
         for (int i = 0; i < abs(n); i++){
             x1.push_back('0');
         }
     }
-    int sum, stock = 0;
-    n = x1.size();
-    for (int i = 0;i < n;i++){
-        int xx = int(x1[i] - 48), yy = int(y1[i] - 48);
+
+    int sum = 0, stock = 0, m = y1.size();
+    for (int i = 0;i<m;i++){
+        int xx = x1[i] - 48, yy = y1[i] - 48;
         sum = xx + yy + stock;
         z1.push_back(char(sum%10 + 48));
         stock = sum/10;
-        //cout<<x1[i]<<" "<<y1[i]<<" "<<z1[i]<<endl;
-        if ((i == n - 1)&&(stock != 0)){
-            z1.push_back(char(stock + 48));
-            //cout<<"    "<<z1[i + 1]<<endl;
-        }
+        //cout<<x1[i]<<" "<<y1[i]<<" "<<z1[i]<<" "<<endl;
     }
-    reverse(z1.begin(), z1.end());
-    char *z = (char *) malloc(sizeof(char)*z1.size()) - 2;
-    for (int i = 0; i < z1.size(); i++){
-        z[i] = z1[i];
-        //cout<<z[i]<<" ";
+    if (stock != 0){
+        z1.push_back(char(stock + 48));
+        //cout<<endl<<z1[m]<<" "<<z1.size()<<endl;
+        m++;
     }
+    reverse(z1.begin(),z1.end());
 
+    char *z = new char[m];
+    for (int i = 0;i<m;i++){
+        z[i] = z1[i];
+        //cout<<z[i];
+    }
     return z;
 }
