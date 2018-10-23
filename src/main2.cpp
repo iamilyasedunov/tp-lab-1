8a37659d8166ea9a3c502ec0e108d89708a97136
iamilyasedunov <iliasedunov@yandex.ru>
2018-10-23 03:20:08 +0300

    B18 with TASK...

--- src/main2.cpp
+++ src/main2.cpp
@@ -3,17 +3,17 @@
 
 int main(){
 	unsigned int value; //проверка числа на простоту
-	std::cout<<"Enter value: "
+	std::cout<<"Enter value: ";
 	std::cin>>value;
 	std::cout<<" "<<checkPrime(value)<<endl;
 	
 	unsigned n; //нахождение n-ого простого числа (в ряду)
-	std::cout<<"Enter n: "
+	std::cout<<"Enter n: ";
 	std::cin>>n;
 	std::cout<<" "<<nPrime(n)<<endl;
 	
 	unsigned int value; //нахождение ближайшего следующего простого числа к value.
-	std::cout<<"Enter value: "
+	std::cout<<"Enter value: ";
 	std::cin>>value;
 	std::cout<<" "<<nextPrime(value)<<endl;
 	return 0;
